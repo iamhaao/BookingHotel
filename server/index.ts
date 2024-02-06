18125764c2a9ec5c683a44a071775f65944d1fb7
iamhaao <haolee2002tst@gmail.com>
2024-02-06 09:28:15 +0700

    Add Layout

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
+mongoose.connect(process.env.MONGODB_CONNECT as string);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
